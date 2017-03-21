205303cb1e1e147259a929a2ee1e71880d3c02d8
ˡᵒᵍᵃⁿ ˡᵉʷᶦˢ <llewis@student.csdspartans.org>
2017-03-21 11:17:22 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -20,7 +20,7 @@ while line != '':
       print(" ")
     else:
       line_counter += 1 
-      print(line_counter, line, end = '')              
+      print(line_counter,")   ", line, end = '')              
     
     line = file_input.readline()
 total_lines_in_file = 32    
@@ -30,39 +30,7 @@ print(" ")
 print("The number of stanzas is: ",stanza_counter)
 print("The number of lines is: ",line_counter)
 print("The total lines in this file: ",total_lines_in_file)
-print("First album was The Magnificent Moodies in 1965 and the members were Ray thomas, John lodge ,and mike pinder")
+print("The members were Ray Thomas, John Lodge, Justin Hayward, Graeme Edge,and Mike Pinder")
+print("The song \"Tuesday Afternoon\" first appeared on the album \x1B[3mDays of Future Passed\x1b[23m in 1967.")
 
 file_input.close()
-
- Tuesday Afternoon												            by The Moody Blues
-
- Tuesday afternoon
- I’m just beginning to see
- Now I'm on my way
- It doesn't matter to me
- Chasing the clouds away
-
- Something calls to me
- The trees are drawing me near
- I've got to find out why
- Those gentle voices I hear
- Explain it all with a sigh
-
-I'm looking at myself reflections of my mind
-It's just the kind of day to leave myself behind
-So gently swaying through the fairyland of love
-If you'll just come with me you'll see the beauty of
-Tuesday afternoon
-Tuesday afternoon
-
-Tuesday afternoon
-I'm just beginning to see
-Now I'm on my way
-It doesn't matter to me
-Chasing the clouds away
-
-Something calls to me
-The trees are drawing me near
-I've got to find out why
-Those gentle voices I hear
-Explain it all with a sigh
